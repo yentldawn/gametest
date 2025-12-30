1fd5a61e7809a2ecd3571cb96ecca007bdcf8870
yentldawn <yentldawn@gmail.com>
2025-12-30 20:35:29 +0530

    appreset

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-<<<<<<< HEAD
+let playerScore = 0;
+let computerScore = 0;
+
 function play(userChoice) {
   const choices = ['rock', 'paper', 'scissors'];
-  const computerChoice = choices[Math.floor(Math.random() * 3)];
+  const computerChoice = choices[Math.floor(Math.random() * choices.length)];
 
   let result = '';
 
@@ -12,31 +14,13 @@ function play(userChoice) {
     (userChoice === 'paper' && computerChoice === 'rock') ||
     (userChoice === 'scissors' && computerChoice === 'paper')
   ) {
-    result = `You win! Computer chose ${computerChoice}`;
+    result = `You win! Computer chose ${computerChoice}.`;
+    playerScore++;
   } else {
-    result = `You lose! Computer chose ${computerChoice}`;
+    result = `You lose! Computer chose ${computerChoice}.`;
+    computerScore++;
   }
 
   document.getElementById('result').innerText = result;
-=======
-function play(userChoice) {
-  const choices = ['rock', 'paper', 'scissors'];
-  const computerChoice = choices[Math.floor(Math.random() * 3)];
-
-  let result = '';
-
-  if (userChoice === computerChoice) {
-    result = "It's a tie!";
-  } else if (
-    (userChoice === 'rock' && computerChoice === 'scissors') ||
-    (userChoice === 'paper' && computerChoice === 'rock') ||
-    (userChoice === 'scissors' && computerChoice === 'paper')
-  ) {
-    result = `You win! Computer chose ${computerChoice}`;
-  } else {
-    result = `You lose! Computer chose ${computerChoice}`;
-  }
-
-  document.getElementById('result').innerText = result;
->>>>>>> 143f8ac22dfab0e97d1ce9f1c5017293dc44ffc3
+  document.getElementById('score').innerText = `Player: ${playerScore} | Computer: ${computerScore}`;
 }
